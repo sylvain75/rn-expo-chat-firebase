81e29d758851283721a3088cb082c7b683e7b422
Sylvain <sylvoz75@gmail.com>
2019-04-25 12:21:46 +1000

    adding basic navigation between main and chat page

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-// Import the screens
+import React, { Component } from 'react';
 import Main from './components/Main';
 import Chat from './components/Chat';
-// Import React Navigation
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
 // Create the navigator
-const navigator = createStackNavigator({
-  Main: { screen: Main },
-  Chat: { screen: Chat },
-});
+const navigator = createStackNavigator(
+  {
+    Main: { screen: Main },
+    Chat: { screen: Chat }
+  },
+  {
+    initialRouteName: 'Main'
+  }
+);
 
 export default createAppContainer(navigator);
-// Export it as the root component
 
--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
 
-const Main = () => {
+const Main = ({ navigation }) => {
+  const [name, setName] = useState('');
+
+  const onChangeText = (newText) => {
+    setName(newText)
+  };
+
+  const onPress = () => {
+    navigation.navigate('Chat', { name });
+  };
+
   return (
     <View>
-      <Text>Main page</Text>
+      <Text style={styles.title}>Enter your name:</Text>
+      <TextInput
+        onChangeText={onChangeText}
+        style={styles.nameInput}
+        placeHolder="BAC 75 N"
+        value={name}
+      />
+      <TouchableOpacity onPress={onPress}>
+        <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
     </View>
   );
 };
-
+const offset = 24;
+const styles = StyleSheet.create({
+  nameInput: {
+    height: offset * 2,
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: '#111111',
+    borderWidth: 1,
+  },
+  title: {
+    marginTop: offset,
+    marginLeft: offset,
+    fontSize: offset,
+  },
+  buttonText: {
+    marginLeft: offset,
+    fontSize: offset,
+  },
+});
 export default Main;
